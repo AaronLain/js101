d53b9fa802c228b3a27c37d1a8669e305e616f5b
Aaron Lain <aaron.j.lain@gmail.com>
2020-01-20 18:58:41 -0600

    added old age and forty two

--- functions.js
+++ functions.js
@@ -13,3 +13,18 @@ const dogBreed = (dog) => {
 }
 
 console.log(dogBreed('lab'));
+
+
+const fortyTwo = (number) => {
+    const addedNumber = number + 42;
+    return `Your new number is ${addedNumber}`;
+}
+
+console.log(fortyTwo(15));
+
+const oldAge = (year) => {
+    const superOld = 2099 - year;
+    return `You will be ${superOld} in 2099`;
+}
+
+console.log(oldAge(1965));
