1e6fff7b5f06b0e0134c37a7445ba19bb04fed4f
Aaron Lain <aaron.j.lain@gmail.com>
2020-01-20 18:47:40 -0600

    added stuff/nuggetizer function

---
+++ functions.js
@@ -0,0 +1,9 @@
+const nuggetizer = (animal) => {
+    const output = `processed ${animal}`;
+    return output;
+};
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('cow'));
+console.log(nuggetizer('fish'));
