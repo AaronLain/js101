0de11c76de646bcb443876b3e97899cb2acf0429
Aaron Lain <aaron.j.lain@gmail.com>
2020-01-20 18:51:44 -0600

    added dogbreed

--- functions.js
+++ functions.js
@@ -7,3 +7,9 @@ console.log(nuggetizer('chicken'));
 console.log(nuggetizer('pig'));
 console.log(nuggetizer('cow'));
 console.log(nuggetizer('fish'));
+
+const dogBreed = (dog) => {
+    return `My favorite dog breed is ${dog}`;
+}
+
+console.log(dogBreed('lab'));
