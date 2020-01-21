f72179b6190dee7ed98269e1b275bdb677659afd
Aaron Lain <aaron.j.lain@gmail.com>
2020-01-20 20:29:05 -0600

    added statusmaker

--- objects.js
+++ objects.js
@@ -18,4 +18,37 @@ const ageCheck = (age) => {
 ageCheck(12);
 ageCheck(22);
 
+const expense = {
+    dateCreated: '01/17/2020',
+    location: 'Baja Burrito',
+    cost: 12.57,
+    isGood: true
+};
+
+console.log(expense.cost);
+
+const employee = {
+    firstName: 'Ben',
+    lastName: 'Swolo',
+    height: 1.8,
+    isCool: false,
+    lightsaberColor: 'red'
+};
+
+console.log(employee.isCool);
+console.log(employee.firstName);
+console.log(employee.lastName);
+
+const statusMaker = (employee) => {
+    if (employee.firstName[0] == 'M') {
+        employee.status = 'vip';
+        return employee;
+    } else {
+        employee.status = 'peasant';
+        return employee;
+    }
+};
+
+statusMaker(employee);
+console.log(employee.status);
 
